c25aa0c2332746128f93d1f681338a388aa4d0cd
Kristin Parrish <kristinparrish@Kristins-MacBook-Pro.local>
2020-02-20 22:20:22 -0700

    added knex file

--- recipes/recipe-model.js
+++ recipes/recipe-model.js
@@ -12,3 +12,36 @@ module.exports = {
 function find(){
     return db("recipes")
 };
+
+function findById(id){
+    return db("schemes")
+        .where({ id })
+        .first();
+};
+
+function add(recipe){
+    return db("recipes")
+        .insert(recipe, "id")
+        .then(id => {
+            return findById(id[0])
+        });
+};
+
+function update(changes, id){
+    return db("recipes")
+        .where({ id })
+        .update(changes)
+        .then(updatedScheme => {
+            return findById(id[0]);
+        });
+};
+
+function remove(id){
+    return db("recipes")
+        .where({ id })
+        .then(found => {
+            return db("recipes")
+            .where({ id })
+            .del()
+        });
+};
